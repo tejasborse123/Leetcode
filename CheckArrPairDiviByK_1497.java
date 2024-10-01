e4e5b389a0207d07b3db40cfcac0b51f766337b0
Tejas Borse <2022bcs519@sggs.ac.in>
2024-10-01 22:30:24 +0530

    Leetcode Question No : 1497 Solution added.

---
+++ CheckArrPairDiviByK_1497.java
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public boolean canArrange(int[] arr, int k) 
+    {
+        int[] remainderFreq = new int[k];
+        
+        for(int num : arr) 
+        {
+            int remainder = (num % k + k) % k;
+            remainderFreq[remainder]++;
+        }
+
+        if (remainderFreq[0] % 2 != 0) return false; 
+        
+        for (int i=1; i<=k/2; i++) 
+        {
+            if (remainderFreq[i] != remainderFreq[k-i]) return false;
+        }
+
+        return true;
+    }
+}
