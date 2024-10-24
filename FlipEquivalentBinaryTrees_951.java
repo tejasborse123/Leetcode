fc721e1b85145d15045512e7ba99fb4ff6fa5054
Tejas Borse <2022bcs519@sggs.ac.in>
2024-10-24 17:19:37 +0530

    Leetcode Question No : 951 Solution added.

---
+++ FlipEquivalentBinaryTrees_951.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) 
+    {
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+        if(root1.val != root2.val) return false;
+
+        boolean isSame = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+        if(isSame) return true;
+      
+        return flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+    }
+}
